7abf48f06131b7e5c267eac7c0bc2b81975223b6
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2012-02-13 01:41:35 +0900

    Fixed typo

--- lib/includes/spdylay/spdylay.h
+++ lib/includes/spdylay/spdylay.h
@@ -400,7 +400,7 @@ void* spdylay_session_get_stream_user_data(spdylay_session *session,
  *
  * "method": HTTP method (e.g., "GET" or "POST")
  * "scheme": URI scheme (e.g., "https")
- * "url": Abosolute path of this request (e.g., "/foo")
+ * "url": Absolute path of this request (e.g., "/foo")
  * "version": HTTP version (e.g., "HTTP/1.1")
  *
  * "host" name/value pair is also required by some hosts.
