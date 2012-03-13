1c09a69d584c979426c140d654b5aaf4036d145f
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2012-03-13 21:36:25 +0900

    Set default_role and domain

--- doc/conf.py
+++ doc/conf.py
@@ -67,7 +67,8 @@ release = '0.1.0'
 exclude_patterns = ['_build']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
-#default_role = None
+default_role = 'c:func'
+primary_domain = 'c'
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
 #add_function_parentheses = True
