424dc2cc9db98db1c0973f018c4cbce24f377405
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2012-08-23 03:37:26 +0900

    python: update setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,12 +1,24 @@
 from distutils.core import setup
 from distutils.extension import Extension
 
-spdylay_dir = '../'
-
 setup(
+    name = 'python-spdylay',
+    version = '0.1.0',
+    description = 'SPDY library',
+    author = 'Tatsuhiro Tsujikawa',
+    author_email = 'tatsuhiro.t@gmail.com',
+    url = 'http://spdylay.sourceforge.net/',
+    keywords = [],
     ext_modules = [Extension("spdylay",
                              ["spdylay.c"],
-                             include_dirs=[spdylay_dir + 'lib/includes'],
-                             library_dirs=[spdylay_dir + 'lib/.libs'],
-                             libraries=['spdylay'])]
+                             libraries=['spdylay'])],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ]
     )
