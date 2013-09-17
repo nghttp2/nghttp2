8dcaa08405dca50ee45193e5da5f6348b59d5670
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2013-09-17 22:48:30 +0900

    Replace steady_clock with monotonic_clock if steady_clock is not available

--- src/nghttp2_config.h
+++ src/nghttp2_config.h
@@ -29,4 +29,10 @@
 #  include <config.h>
 #endif // HAVE_CONFIG_H
 
+// gcc 4.6 has std::chrono::monotonic_clock, which was renamed as
+// std::chrono::steady_clock in C++11 standard.
+#ifndef HAVE_STEADY_CLOCK
+#  define steady_clock monotonic_clock
+#endif // !HAVE_STEADY_CLOCK
+
 #endif // NGHTTP2_CONFIG_H
