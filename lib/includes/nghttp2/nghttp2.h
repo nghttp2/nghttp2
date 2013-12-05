84d1fffb3f5c99faacada7aed65e7429974415d0
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2013-12-05 23:23:39 +0900

    Announce HTTP-draft-09/2.0

--- lib/includes/nghttp2/nghttp2.h
+++ lib/includes/nghttp2/nghttp2.h
@@ -40,7 +40,7 @@ extern "C" {
  *
  * The protocol version identification of this library supports.
  */
-#define NGHTTP2_PROTO_VERSION_ID "HTTP-draft-08/2.0"
+#define NGHTTP2_PROTO_VERSION_ID "HTTP-draft-09/2.0"
 /**
  * @macro
  *
