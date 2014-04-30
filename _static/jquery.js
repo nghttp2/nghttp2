612c704c510e2c97229c793e5d667fb28dafe55e
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2014-04-30 10:27:19 +0900

    Update doc

--- _static/jquery.js
+++ _static/jquery.js
@@ -11,7 +11,7 @@
  * Copyright 2011, The Dojo Foundation
  * Released under the MIT, BSD, and GPL Licenses.
  *
- * Date: Thu Aug  1 23:20:30 BRT 2013
+ * Date: Fri Jul  5 14:07:58 UTC 2013
  */
 (function( window, undefined ) {
 
