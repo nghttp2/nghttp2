5ce8ae79f0ea615da220e04f90b9c0f1d3d1a71d
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2014-11-14 23:16:06 +0900

    nghttpx: Disable spdy/3 and spdy/2 by default

--- src/shrpx.cc
+++ src/shrpx.cc
@@ -673,7 +673,7 @@ bool conf_exists(const char *path)
 namespace {
 const char *DEFAULT_NPN_LIST = NGHTTP2_PROTO_VERSION_ID ","
 #ifdef HAVE_SPDYLAY
-  "spdy/3.1,spdy/3,spdy/2,"
+  "spdy/3.1,"
 #endif // HAVE_SPDYLAY
   "http/1.1";
 } // namespace
