75a1ad8bdb46e3e889da2116ccba5f9af70b44e7
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2014-11-16 16:45:56 +0900

    src: format_iso8601: Fix invalid millisecond formatting

--- src/util.cc
+++ src/util.cc
@@ -719,7 +719,7 @@ std::string format_iso8601(const std::chrono::system_clock::time_point& tp)
   char buf[128];
 
   auto nwrite = strftime(buf, sizeof(buf), "%Y-%m-%dT%H:%M:%S", &tms);
-  snprintf(&buf[nwrite], sizeof(buf) - nwrite, ".%ldZ", t.count() % 1000);
+  snprintf(&buf[nwrite], sizeof(buf) - nwrite, ".%03ldZ", t.count() % 1000);
 
   return buf;
 }
