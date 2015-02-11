nghttp2/nghttp2

commit 795a22a3206f357c79d77de6207a677978f9c79f

src: Remove ringbuf.h, use buffer.h instead

src/h2load.cc
@@ -516,11 +516,8 @@ int Client::read_clear() {
 int Client::write_clear() {
   for (;;) {
     if (wb.rleft() > 0) {
-      struct iovec iov[2];
-      auto iovcnt = wb.riovec(iov);
-
       ssize_t nwrite;
-      while ((nwrite = writev(fd, iov, iovcnt)) == -1 && errno == EINTR)
+      while ((nwrite = write(fd, wb.pos, wb.rleft())) == -1 && errno == EINTR)
         ;
       if (nwrite == -1) {
         if (errno == EAGAIN || errno == EWOULDBLOCK) {
@@ -642,11 +639,7 @@ int Client::write_tls() {
 
   for (;;) {
     if (wb.rleft() > 0) {
-      const void *p;
-      size_t len;
-      std::tie(p, len) = wb.get();
-
-      auto rv = SSL_write(ssl, p, len);
+      auto rv = SSL_write(ssl, wb.pos, wb.rleft());
 
       if (rv == 0) {
         return -1;

src/nghttp.cc
@@ -652,11 +652,8 @@ int HttpClient::write_clear() {
 
   for (;;) {
     if (wb.rleft() > 0) {
-      struct iovec iov[2];
-      auto iovcnt = wb.riovec(iov);
-
       ssize_t nwrite;
-      while ((nwrite = writev(fd, iov, iovcnt)) == -1 && errno == EINTR)
+      while ((nwrite = write(fd, wb.pos, wb.rleft())) == -1 && errno == EINTR)
         ;
       if (nwrite == -1) {
         if (errno == EAGAIN || errno == EWOULDBLOCK) {
@@ -1147,11 +1144,7 @@ int HttpClient::write_tls() {
 
   for (;;) {
     if (wb.rleft() > 0) {
-      const void *p;
-      size_t len;
-      std::tie(p, len) = wb.get();
-
-      auto rv = SSL_write(ssl, p, len);
+      auto rv = SSL_write(ssl, wb.pos, wb.rleft());
 
       if (rv == 0) {
         return -1;

src/HttpServer.cc
@@ -434,11 +434,9 @@ int Http2Handler::write_clear() {
   auto loop = sessions_->get_loop();
   for (;;) {
     if (wb_.rleft() > 0) {
-      struct iovec iov[2];
-      auto iovcnt = wb_.riovec(iov);
-
       ssize_t nwrite;
-      while ((nwrite = writev(fd_, iov, iovcnt)) == -1 && errno == EINTR)
+      while ((nwrite = write(fd_, wb_.pos, wb_.rleft())) == -1 &&
+             errno == EINTR)
         ;
       if (nwrite == -1) {
         if (errno == EAGAIN || errno == EWOULDBLOCK) {
@@ -562,11 +560,7 @@ int Http2Handler::write_tls() {
 
   for (;;) {
     if (wb_.rleft() > 0) {
-      const void *p;
-      size_t len;
-      std::tie(p, len) = wb_.get();
-
-      auto rv = SSL_write(ssl_, p, len);
+      auto rv = SSL_write(ssl_, wb_.pos, wb_.rleft());
 
       if (rv == 0) {
         return -1;

src/HttpServer.h
@@ -44,7 +44,7 @@
 #include <nghttp2/nghttp2.h>
 
 #include "http2.h"
-#include "ringbuf.h"
+#include "buffer.h"
 
 namespace nghttp2 {
 
@@ -143,7 +143,7 @@ private:
   ev_io rev_;
   ev_timer settings_timerev_;
   std::map<int32_t, std::unique_ptr<Stream>> id2stream_;
-  RingBuf<65536> wb_;
+  Buffer<65536> wb_;
   std::function<int(Http2Handler &)> read_, write_;
   int64_t session_id_;
   nghttp2_session *session_;

src/Makefile.am
@@ -76,8 +76,7 @@ nghttp_SOURCES = ${HELPER_OBJECTS} ${HELPER_HFILES} nghttp.cc nghttp.h \
 
 nghttpd_SOURCES = ${HELPER_OBJECTS} ${HELPER_HFILES} nghttpd.cc \
 	ssl.cc ssl.h \
-	HttpServer.cc HttpServer.h \
-	ringbuf.h
+	HttpServer.cc HttpServer.h
 
 bin_PROGRAMS += h2load
 
@@ -142,7 +141,6 @@ nghttpx_unittest_SOURCES = shrpx-unittest.cc \
 	util_test.cc util_test.h \
 	nghttp2_gzip_test.c nghttp2_gzip_test.h \
 	nghttp2_gzip.c nghttp2_gzip.h \
-	ringbuf_test.cc ringbuf_test.h \
 	buffer_test.cc buffer_test.h \
 	memchunk_test.cc memchunk_test.h
 nghttpx_unittest_CPPFLAGS = ${AM_CPPFLAGS}\

src/buffer_test.cc
@@ -45,7 +45,7 @@ void test_buffer_write(void) {
 
   CU_ASSERT(3 == b.rleft());
   CU_ASSERT(13 == b.wleft());
-  CU_ASSERT(0 == b.pos - std::begin(b.buf));
+  CU_ASSERT(b.pos == std::begin(b.buf));
 
   b.drain(3);
 
@@ -65,14 +65,14 @@ void test_buffer_write(void) {
   b.reset();
 
   CU_ASSERT(0 == b.rleft());
-  CU_ASSERT(0 == b.wleft());
-  CU_ASSERT(0 == b.pos - std::begin(b.buf));
+  CU_ASSERT(16 == b.wleft());
+  CU_ASSERT(b.pos == std::begin(b.buf));
 
   b.write(5);
 
   CU_ASSERT(5 == b.rleft());
   CU_ASSERT(11 == b.wleft());
-  CU_ASSERT(0 == b.pos - std::begin(b.buf));
+  CU_ASSERT(b.pos == std::begin(b.buf));
 }
 
 } // namespace nghttp2

src/h2load.h
@@ -45,7 +45,7 @@
 #include <openssl/ssl.h>
 
 #include "http2.h"
-#include "ringbuf.h"
+#include "buffer.h"
 
 using namespace nghttp2;
 
@@ -174,7 +174,7 @@ struct Client {
   // The number of requests this client has done so far.
   size_t req_done;
   int fd;
-  RingBuf<65536> wb;
+  Buffer<65536> wb;
 
   enum { ERR_CONNECT_FAIL = -100 };
 

src/nghttp.h
@@ -45,7 +45,7 @@
 
 #include "http-parser/http_parser.h"
 
-#include "ringbuf.h"
+#include "buffer.h"
 #include "http2.h"
 #include "nghttp2_gzip.h"
 
@@ -261,7 +261,7 @@ struct HttpClient {
   // true if the response message of HTTP Upgrade request is fully
   // received. It is not relevant the upgrade succeeds, or not.
   bool upgrade_response_complete;
-  RingBuf<65536> wb;
+  Buffer<65536> wb;
   // SETTINGS payload sent as token68 in HTTP Upgrade
   std::array<uint8_t, 128> settings_payload;
 

commit 756e2b3e9b303b03bb2bc0af26b4ba1586925135

nghttp, h2load: Fix regression introduced in 795a22a
We missed wb.reset(), which makes write buffer's capacity becomes 0
and communication stalls eventually.

src/h2load.cc
@@ -534,12 +534,11 @@ int Client::write_clear() {
       wb.drain(nwrite);
       continue;
     }
-
+    wb.reset();
     if (on_write() != 0) {
       return -1;
     }
     if (wb.rleft() == 0) {
-      wb.reset();
       break;
     }
   }
@@ -668,6 +667,7 @@ int Client::write_tls() {
 
       continue;
     }
+    wb.reset();
     if (on_write() != 0) {
       return -1;
     }

src/nghttp.cc
@@ -666,12 +666,11 @@ int HttpClient::write_clear() {
       wb.drain(nwrite);
       continue;
     }
-
+    wb.reset();
     if (on_writefn(*this) != 0) {
       return -1;
     }
     if (wb.rleft() == 0) {
-      wb.reset();
       break;
     }
   }
@@ -1169,6 +1168,7 @@ int HttpClient::write_tls() {
 
       continue;
     }
+    wb.reset();
     if (on_writefn(*this) != 0) {
       return -1;
     }