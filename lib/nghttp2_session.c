87602e5d7296b37ed3f2864db1965d139657aba6
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2015-04-05 22:46:05 +0900

    Use NGHTTP2_PROTOCOL_ERROR for NGHTTP2_ERR_HTTP_{HEADER,MESSAGING}

--- lib/nghttp2_session.c
+++ lib/nghttp2_session.c
@@ -3170,6 +3170,8 @@ static int get_error_code_from_lib_error_code(int lib_error_code) {
   case NGHTTP2_ERR_REFUSED_STREAM:
     return NGHTTP2_REFUSED_STREAM;
   case NGHTTP2_ERR_PROTO:
+  case NGHTTP2_ERR_HTTP_HEADER:
+  case NGHTTP2_ERR_HTTP_MESSAGING:
     return NGHTTP2_PROTOCOL_ERROR;
   default:
     return NGHTTP2_INTERNAL_ERROR;
