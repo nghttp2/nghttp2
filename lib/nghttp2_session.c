7323d4c639d703767c092301cf75f5489e0b191b
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2015-04-17 00:12:47 +0900

    Fix bug that nghttp2_session_set_next_stream_id accepts invalid stream_id

--- lib/nghttp2_session.c
+++ lib/nghttp2_session.c
@@ -6622,8 +6622,16 @@ int nghttp2_session_consume_stream(nghttp2_session *session, int32_t stream_id,
 
 int nghttp2_session_set_next_stream_id(nghttp2_session *session,
                                        int32_t next_stream_id) {
-  if (next_stream_id < 0 ||
+  if (next_stream_id <= 0 ||
       session->next_stream_id > (uint32_t)next_stream_id) {
+    return NGHTTP2_ERR_INVALID_ARGUMENT;
+  }
+
+  if (session->server) {
+    if (next_stream_id % 2) {
+      return NGHTTP2_ERR_INVALID_ARGUMENT;
+    }
+  } else if (next_stream_id % 2 == 0) {
     return NGHTTP2_ERR_INVALID_ARGUMENT;
   }
 
