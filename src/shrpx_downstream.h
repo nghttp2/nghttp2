77c556901cac2deb6dd94a7498edb89ff2543638
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2015-04-22 22:26:04 +0900

    nghttpx: Increase maximum header field set size

--- src/shrpx_downstream.h
+++ src/shrpx_downstream.h
@@ -287,7 +287,7 @@ public:
   int change_priority(int32_t pri);
 
   // Maximum buffer size for header name/value pairs.
-  static const size_t MAX_HEADERS_SUM = 32768;
+  static constexpr size_t MAX_HEADERS_SUM = 128 * 1024;
 
   bool get_rst_stream_after_end_stream() const;
   void set_rst_stream_after_end_stream(bool f);
