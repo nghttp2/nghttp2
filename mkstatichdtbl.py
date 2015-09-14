230b1f927f30cfab305e0a2f8ba367a6b6e01686
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2015-09-15 01:05:41 +0900

    Use hash table for dynamic table lookup

--- mkstatichdtbl.py
+++ mkstatichdtbl.py
@@ -10,6 +10,17 @@
 from __future__ import unicode_literals
 import re, sys
 
+def hd_map_hash(name):
+  h = 2166136261
+
+  # FNV hash variant: http://isthe.com/chongo/tech/comp/fnv/
+  for c in name:
+    h ^= ord(c)
+    h *= 16777619
+    h &= 0xffffffff
+
+  return h
+
 entries = []
 for line in sys.stdin:
     m = re.match(r'(\d+)\s+(\S+)\s+(\S.*)?', line)
@@ -21,6 +32,6 @@ idx = 0
 for i, ent in enumerate(entries):
     if entries[idx][1] != ent[1]:
         idx = i
-    print 'MAKE_STATIC_ENT("{}", "{}", {}),'\
-        .format(ent[1], ent[2], entries[idx][0] - 1)
+    print 'MAKE_STATIC_ENT("{}", "{}", {}, {}u),'\
+        .format(ent[1], ent[2], entries[idx][0] - 1, hd_map_hash(ent[1]))
 print '};'
