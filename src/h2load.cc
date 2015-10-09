a5070ba6adb921e48ce1a6418952aff46fca491a
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2015-10-09 19:06:43 +0900

    Special handling of -n when used with --timing-script
    If -n is used with --timing-script, it specifies the number of
    requests each client will make rather than the total number of
    requests h2load will perform across clients.

--- src/h2load.cc
+++ src/h2load.cc
@@ -1291,7 +1291,10 @@ void print_help(std::ostream &out) {
               scheme, host or port values.
 Options:
   -n, --requests=<N>
-              Number of requests.
+              Number of  requests across all  clients.  If it  is used
+              with --timing-script-file option,  this option specifies
+              the number of requests  each client performs rather than
+              the number of requests across all clients.
               Default: )" << config.nreqs << R"(
   -c, --clients=<N>
               Number  of concurrent  clients.   With  -r option,  this
@@ -1377,28 +1380,22 @@ Options:
               inactive),   h2load   will   keep  a   connection   open
               indefinitely, waiting for a response.
   --timing-script-file=<PATH>
-              Path of a file containing one or more lines separated by
-              EOLs. Each script line  is composed of two tab-separated
-              fields. The first field  represents the time offset from
-              the start of execution, expressed as a positive value of
-              milliseconds  with microsecond  resolution.  The  second
-              field represents the URI.  This option will disable URIs
-              getting from  command-line.  If '-' is  given as <PATH>,
-              script lines will be read  from stdin.  Script lines are
-              used in order for each client.   If -n is given, it must
-              be  less than  or equal  to the  number of  script lines
-              multiplied by number of  clients (see -c option), larger
-              values are clamped to the latter.  The multiplication is
-              done in  order to mimic  -n option semantics  where each
-              client does -n / -c  requests.  With this timing script,
-              we'd like  to make each  client issue whole  sequence of
-              URIs if -n is not given.  If -n is not given, the number
-              of requests will  default to the number  of script lines
-              multiplied by  number of clients.  The  scheme, host and
-              port defined in  the first URI are  used solely.  Values
-              contained  in  other  URIs,  if  present,  are  ignored.
-              Definition of a  base URI overrides all  scheme, host or
-              port values.
+              Path of a file containing one  or more lines separated by
+              EOLs. Each script line  is composed of  two tab-separated
+              fields. The first field  represents  the time offset from
+              the start of execution,  expressed as a positive value of
+              milliseconds  with  microsecond  resolution.  The  second
+              field represents the URI.  This option will disable  URIs
+              getting  from  command-line.  If '-'  is given as <PATH>,
+              script  lines  will be read from stdin.  Script lines are
+              used in order for each client. If -n is given, it must be
+              less than or equal to the number of script lines,  larger
+              values are clamped to the number of script lines.  If  -n
+              is not given,  the number of requests will default to the
+              number of script lines. The scheme, host and port defined
+              in the  first URI  are used  solely.  Values contained in
+              other URIs, if  present, are  ignored.  Definition  of  a
+              base  URI  overrides  all  scheme, host  or port  values.
   -B, --base-uri=<URI>
               Specify URI from which the scheme, host and port will be
               used  for  all requests.   The  base  URI overrides  all
@@ -1691,19 +1688,16 @@ int main(int argc, char **argv) {
         read_script_from_file(infile, config.timings, uris);
       }
 
-      // each client will execute the full script, so scale nreqs
-      auto min_nreqs = uris.size() * config.nclients;
       if (nreqs_set_manually) {
-        if (config.nreqs > min_nreqs) {
+        if (config.nreqs > uris.size()) {
           std::cerr << "-n: the number of requests must be less than or equal "
-                       "to the number of timing script entries * number of "
-                       "clients. Setting number "
-                       "of requests to " << min_nreqs << std::endl;
+                       "to the number of timing script entries. Setting number "
+                       "of requests to " << uris.size() << std::endl;
 
-          config.nreqs = min_nreqs;
+          config.nreqs = uris.size();
         }
       } else {
-        config.nreqs = min_nreqs;
+        config.nreqs = uris.size();
       }
     }
 
@@ -1743,7 +1737,9 @@ int main(int argc, char **argv) {
               << "cores." << std::endl;
   }
 
-  if (config.nreqs < config.nclients) {
+  // With timing script, we don't distribute config.nreqs to each
+  // client or thread.
+  if (!config.timing_script && config.nreqs < config.nclients) {
     std::cerr << "-n, -c: the number of requests must be greater than or "
               << "equal to the clients." << std::endl;
     exit(EXIT_FAILURE);
@@ -1906,8 +1902,13 @@ int main(int argc, char **argv) {
 
   resolve_host();
 
-  size_t nreqs_per_thread = config.nreqs / config.nthreads;
-  ssize_t nreqs_rem = config.nreqs % config.nthreads;
+  size_t nreqs_per_thread = 0;
+  ssize_t nreqs_rem = 0;
+
+  if (!config.timing_script) {
+    nreqs_per_thread = config.nreqs / config.nthreads;
+    nreqs_rem = config.nreqs % config.nthreads;
+  }
 
   size_t nclients_per_thread = config.nclients / config.nthreads;
   ssize_t nclients_rem = config.nclients % config.nthreads;
@@ -1935,10 +1936,20 @@ int main(int argc, char **argv) {
       --nclients_rem;
       ++nclients;
     }
-    auto nreqs = nreqs_per_thread;
-    if (nreqs_rem > 0) {
-      --nreqs_rem;
-      ++nreqs;
+
+    size_t nreqs;
+    if (config.timing_script) {
+      // With timing script, each client issues config.nreqs requests.
+      // We divide nreqs by number of clients in Worker ctor to
+      // distribute requests to those clients evenly, so multiply
+      // config.nreqs here by config.nclients.
+      nreqs = config.nreqs * nclients;
+    } else {
+      nreqs = nreqs_per_thread;
+      if (nreqs_rem > 0) {
+        --nreqs_rem;
+        ++nreqs;
+      }
     }
 
     std::stringstream rate_report;
@@ -1966,7 +1977,8 @@ int main(int argc, char **argv) {
   {
     auto rate_last = rate_per_thread;
     auto nclients_last = nclients_per_thread;
-    auto nreqs_last = nreqs_per_thread;
+    auto nreqs_last =
+        config.timing_script ? config.nreqs * nclients_last : nreqs_per_thread;
     std::stringstream rate_report;
     if (config.is_rate_mode() && nclients_last > config.rate) {
       rate_report << "Up to " << rate_last
