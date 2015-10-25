4960583637e20bee729de7f06f80c0798e533d7b
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2015-10-25 16:23:29 +0900

    Increase NGHTTP2_MAX_OBQ_FLOOD_ITEM to avoid false positives

--- lib/nghttp2_session.h
+++ lib/nghttp2_session.h
@@ -81,7 +81,7 @@ typedef struct {
    response frames are stacked up, which leads to memory exhaustion.
    The value selected here is arbitrary, but safe value and if we have
    these frames in this number, it is considered suspicious. */
-#define NGHTTP2_MAX_OBQ_FLOOD_ITEM 1000
+#define NGHTTP2_MAX_OBQ_FLOOD_ITEM 10000
 
 /* Internal state when receiving incoming frame */
 typedef enum {
