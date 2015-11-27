3d1d54e2ced302f2ded012e154436fae3b855f98
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2015-11-27 21:13:44 +0900

    Remove dead code

--- lib/nghttp2_session.c
+++ lib/nghttp2_session.c
@@ -3361,8 +3361,6 @@ int nghttp2_session_end_headers_received(nghttp2_session *session,
                                          nghttp2_stream *stream) {
   int rv;
   if (frame->hd.flags & NGHTTP2_FLAG_END_STREAM) {
-    if (!nghttp2_session_is_my_stream_id(session, frame->hd.stream_id)) {
-    }
     nghttp2_stream_shutdown(stream, NGHTTP2_SHUT_RD);
     rv = nghttp2_session_close_stream_if_shut_rdwr(session, stream);
     if (nghttp2_is_fatal(rv)) {
