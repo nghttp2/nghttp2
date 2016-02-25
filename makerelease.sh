d1f9f9a3aa2c7208ddc3b4e4b0964099c4b57a3e
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2016-02-26 00:00:52 +0900

    makerelease.sh: Add autoreconf

--- makerelease.sh
+++ makerelease.sh
@@ -9,6 +9,8 @@ git checkout refs/tags/$TAG
 git log --pretty=fuller --date=short refs/tags/$PREV_TAG..HEAD > ChangeLog
 
 git submodule update --init
+
+autoreconf -i
 ./configure --with-mruby && \
     make dist-bzip2 && make dist-gzip && make dist-xz || echo "error"
 make distclean
