nghttp2/nghttp2

commit 5fbe4cc2257d95dfa51c5dd8969b3f5201e0f9db

nghttpx: Clear upgrade_request flag when Downstream is attached to HTTP/2 backend

src/shrpx_http2_downstream_connection.cc
@@ -105,6 +105,11 @@ int Http2DownstreamConnection::attach_downstream(Downstream *downstream) {
   downstream_ = downstream;
   downstream_->reset_downstream_rtimer();
 
+  auto &req = downstream_->request();
+
+  // HTTP/2 disables HTTP Upgrade.
+  req.upgrade_request = false;
+
   return 0;
 }
 

commit ff070187203d77352b4e085c623242cb49342882

nghttpx: Fix bug that proxy with HTTP/1.1 CONNECT did not work
This was a regression in 5fbe4cc2257d95dfa51c5dd8969b3f5201e0f9db.

src/shrpx_http2_downstream_connection.cc
@@ -108,7 +108,9 @@ int Http2DownstreamConnection::attach_downstream(Downstream *downstream) {
   auto &req = downstream_->request();
 
   // HTTP/2 disables HTTP Upgrade.
-  req.upgrade_request = false;
+  if (req.method != HTTP_CONNECT) {
+    req.upgrade_request = false;
+  }
 
   return 0;
 }