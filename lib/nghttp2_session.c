8667bbb82328b61ac259010b2a2525f0beafabed
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2016-03-29 23:30:55 +0900

    Don't send WINDOW_UPDATE if session is being closed

--- lib/nghttp2_session.c
+++ lib/nghttp2_session.c
@@ -1642,6 +1642,11 @@ static int session_predicate_push_promise_send(nghttp2_session *session,
 static int session_predicate_window_update_send(nghttp2_session *session,
                                                 int32_t stream_id) {
   nghttp2_stream *stream;
+
+  if (session_is_closing(session)) {
+    return NGHTTP2_ERR_SESSION_CLOSING;
+  }
+
   if (stream_id == 0) {
     /* Connection-level window update */
     return 0;
@@ -1649,9 +1654,6 @@ static int session_predicate_window_update_send(nghttp2_session *session,
   stream = nghttp2_session_get_stream(session, stream_id);
   if (stream == NULL) {
     return NGHTTP2_ERR_STREAM_CLOSED;
-  }
-  if (session_is_closing(session)) {
-    return NGHTTP2_ERR_SESSION_CLOSING;
   }
   if (stream->state == NGHTTP2_STREAM_CLOSING) {
     return NGHTTP2_ERR_STREAM_CLOSING;
