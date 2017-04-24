67d5b6821b13c8fb3bc7c0ed3a25b98d4f103de7
Lucas Pardue <lucas.pardue@bbc.co.uk>
2017-04-24 18:09:16 +0100

    ORIGIN frame support

--- lib/nghttp2_submit.c
+++ lib/nghttp2_submit.c
@@ -486,6 +486,76 @@ int nghttp2_session_set_local_window_size(nghttp2_session *session,
   return 0;
 }
 
+int nghttp2_submit_origin(nghttp2_session *session, uint8_t flags,
+                          const uint8_t *origin, size_t origin_len) {
+  nghttp2_mem *mem;
+  uint8_t *buf, *p;
+  uint8_t *origin_copy;
+  nghttp2_outbound_item *item;
+  nghttp2_frame *frame;
+  nghttp2_ext_origin *origin_frame;
+  int rv;
+  (void)flags;
+
+  mem = &session->mem;
+
+  if (!session->server) {
+    return NGHTTP2_ERR_INVALID_STATE;
+  }
+
+  if (2 + origin_len > NGHTTP2_MAX_PAYLOADLEN) {
+    return NGHTTP2_ERR_INVALID_ARGUMENT;
+  }
+
+  buf = nghttp2_mem_malloc(mem, origin_len + 2);
+  if (buf == NULL) {
+    return NGHTTP2_ERR_NOMEM;
+  }
+
+  p = buf;
+
+  origin_copy = p;
+  if (origin_len) {
+    p = nghttp2_cpymem(p, origin, origin_len);
+  }
+  *p++ = '\0';
+
+  item = nghttp2_mem_malloc(mem, sizeof(nghttp2_outbound_item));
+  if (item == NULL) {
+    rv = NGHTTP2_ERR_NOMEM;
+    goto fail_item_malloc;
+  }
+
+  nghttp2_outbound_item_init(item);
+
+  item->aux_data.ext.builtin = 1;
+
+  origin_frame = &item->ext_frame_payload.origin;
+
+  frame = &item->frame;
+  frame->ext.payload = origin_frame;
+  //
+  //  nghttp2_frame_altsvc_init(&frame->ext, stream_id, origin_copy, origin_len,
+  //                            field_value_copy, field_value_len);
+
+  nghttp2_frame_origin_init(&frame->ext, origin_copy, origin_len);
+
+  rv = nghttp2_session_add_item(session, item);
+  if (rv != 0) {
+    nghttp2_frame_altsvc_free(&frame->ext, mem);
+    nghttp2_mem_free(mem, item);
+
+    return rv;
+  }
+
+  return 0;
+
+fail_item_malloc:
+  free(buf);
+
+  return rv;
+}
+
 int nghttp2_submit_altsvc(nghttp2_session *session, uint8_t flags,
                           int32_t stream_id, const uint8_t *origin,
                           size_t origin_len, const uint8_t *field_value,
