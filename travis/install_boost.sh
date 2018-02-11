d503ce60e03f72383916fef44ec59bd7a5592a4d
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2018-02-12 01:18:49 +0300

    Separate build Boost script

---
+++ travis/install_boost.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+_startup_dir=`pwd`
+
+export CI_BOOST_ROOT=${HOME}/boost
+
+# Download Boost
+mkdir -p ${HOME}/boost_source
+CI_BOOST_VERSION_=`echo -n ${CI_BOOST_VERSION} | sed 's/\./_/g'`
+CI_BOOST_URL="https://dl.bintray.com/boostorg/release/${CI_BOOST_VERSION}/source/boost_${CI_BOOST_VERSION_}.tar.gz"
+travis_retry wget --no-check-certificate --quiet -O - ${CI_BOOST_URL} | tar --strip-components=1 -xz -C ${HOME}/boost_source
+# Build Boost
+mkdir -p ${HOME}/boost/include
+cd ${HOME}/boost_source
+#  - "if [[ "$CC" = "gcc" ]]; then echo -n \"using gcc \: \: g++-7 ;\" > tools/build/src/user-config.jam; fi"
+#  - "if [[ "$CC" = "clang" ]]; then echo -n \"using clang \: \: clang++ ;\" > tools/build/src/user-config.jam; fi"
+echo -n \"using clang : : clang++ ;\" > tools/build/src/user-config.jam
+./bootstrap.sh
+CI_B2_OPTIONS="--with-system --with-thread --with-date_time --with-regex --with-serialization --build-type=minimal --stagedir=${CI_BOOST_ROOT}"
+CI_B2_PROPERTIES="threading=multi link=static variant=${CI_BUILD_TYPE,,} cflags=-fPIC cxxflags=-fPIC"
+./b2 ${CI_B2_OPTIONS} ${CI_B2_PROPERTIES} stage
+mv ${HOME}/boost_source/boost ${CI_BOOST_ROOT}/include/
+
+cd ${_startup_dir}
+unset ${_startup_dir}
+
