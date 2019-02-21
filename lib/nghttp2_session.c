5bf12b24f985ffce9a40b9ce5e173d3fce696178
Yang Song <yasong@google.com>
2019-02-21 15:12:46 -0500

    Move the parse of session->ob_reg after session->ob_syn.

--- lib/nghttp2_session.c
+++ lib/nghttp2_session.c
@@ -2349,13 +2349,6 @@ nghttp2_session_pop_next_ob_item(nghttp2_session *session) {
     return item;
   }
 
-  item = nghttp2_outbound_queue_top(&session->ob_reg);
-  if (item) {
-    nghttp2_outbound_queue_pop(&session->ob_reg);
-    item->queued = 0;
-    return item;
-  }
-
   if (!session_is_outgoing_concurrent_streams_max(session)) {
     item = nghttp2_outbound_queue_top(&session->ob_syn);
     if (item) {
@@ -2363,6 +2356,13 @@ nghttp2_session_pop_next_ob_item(nghttp2_session *session) {
       item->queued = 0;
       return item;
     }
+  }
+
+  item = nghttp2_outbound_queue_top(&session->ob_reg);
+  if (item) {
+    nghttp2_outbound_queue_pop(&session->ob_reg);
+    item->queued = 0;
+    return item;
   }
 
   if (session->remote_window_size > 0) {
