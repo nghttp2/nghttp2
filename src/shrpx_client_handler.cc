7b645c5d98fa83941432ec7ae785c7ff84952f0d
Sachio Tomita <sachio.tomita@gmail.com>
2019-03-15 16:58:40 -0700

    Patch for clang bug

--- src/shrpx_client_handler.cc
+++ src/shrpx_client_handler.cc
@@ -958,7 +958,7 @@ ClientHandler::get_downstream_connection(int &err, Downstream *downstream) {
     auto dconn = addr->dconn_pool->pop_downstream_connection();
     if (dconn) {
       dconn->set_client_handler(this);
-      return dconn;
+      return std::move(dconn);
     }
 
     if (LOG_ENABLED(INFO)) {
@@ -969,7 +969,7 @@ ClientHandler::get_downstream_connection(int &err, Downstream *downstream) {
     dconn = std::make_unique<HttpDownstreamConnection>(group, addr, conn_.loop,
                                                        worker_);
     dconn->set_client_handler(this);
-    return dconn;
+    return std::move(dconn);
   }
 
   if (LOG_ENABLED(INFO)) {
@@ -980,7 +980,7 @@ ClientHandler::get_downstream_connection(int &err, Downstream *downstream) {
   auto http2session = get_http2_session(group, addr);
   auto dconn = std::make_unique<Http2DownstreamConnection>(http2session);
   dconn->set_client_handler(this);
-  return dconn;
+  return std::move(dconn);
 }
 
 MemchunkPool *ClientHandler::get_mcpool() { return worker_->get_mcpool(); }
