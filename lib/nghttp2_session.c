dc8ceed2cb39344f458087964aee181a635b79df
asraa <asraa@google.com>
2019-11-22 15:46:38 -0500

    Update nghttp2_session.c

--- lib/nghttp2_session.c
+++ lib/nghttp2_session.c
@@ -5350,9 +5350,6 @@ static ssize_t inbound_frame_effective_readlen(nghttp2_inbound_frame *iframe,
 ssize_t nghttp2_session_mem_recv(nghttp2_session *session, const uint8_t *in,
                                  size_t inlen) {
   const uint8_t *first = in, *last = in;
-  if (in != NULL) {
-    last += inlen;
-  }
   nghttp2_inbound_frame *iframe = &session->iframe;
   size_t readlen;
   ssize_t padlen;
@@ -5362,6 +5359,10 @@ ssize_t nghttp2_session_mem_recv(nghttp2_session *session, const uint8_t *in,
   nghttp2_stream *stream;
   size_t pri_fieldlen;
   nghttp2_mem *mem;
+  
+  if (in != NULL) {
+    last += inlen;
+  }
 
   DEBUGF("recv: connection recv_window_size=%d, local_window=%d\n",
          session->recv_window_size, session->local_window_size);
