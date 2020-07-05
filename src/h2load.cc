b8c63247ebc6697fb7769e9fc94dcc91ac1b5b22
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2020-07-05 18:58:59 +0900

    QUIC needs termination without session

--- src/h2load.cc
+++ src/h2load.cc
@@ -784,7 +784,9 @@ void Client::terminate_session() {
   if (config.is_quic()) {
     quic.close_requested = true;
   }
-  session->terminate();
+  if (session) {
+    session->terminate();
+  }
   // http1 session needs writecb to tear down session.
   signal_write();
 }
@@ -1417,7 +1419,7 @@ Worker::~Worker() {
 
 void Worker::stop_all_clients() {
   for (auto client : clients) {
-    if (client && client->session) {
+    if (client) {
       client->terminate_session();
     }
   }
