nghttp2/nghttp2

commit fdcdb21c38ef7acf23853d47d1dc5bda69d5398f

nghttpx: Randomize backend address round robin order per thread

src/shrpx_worker.cc
@@ -304,6 +304,9 @@ void Worker::replace_downstream_config(
       shared_addr->http1_pri.weight = num_http1;
       shared_addr->http2_pri.weight = num_http2;
 
+      std::shuffle(std::begin(shared_addr->addrs), std::end(shared_addr->addrs),
+                   randgen_);
+
       if (shared_addr->affinity.type != SessionAffinity::NONE) {
         for (auto &addr : shared_addr->addrs) {
           addr.dconn_pool = std::make_unique<DownstreamConnectionPool>();

commit 470f06e5f6deab5284ffe93f15cc4ce8b16ee2ec

nghttpx: Fix broken session affinity
Session affinity has been broken since
fdcdb21c38ef7acf23853d47d1dc5bda69d5398f.  This commit reverts the
relevant part of the commit to fix the session affinity.

src/shrpx_worker.cc
@@ -333,9 +333,6 @@ void Worker::replace_downstream_config(
     auto it = addr_groups_indexer.find(dkey);
 
     if (it == std::end(addr_groups_indexer)) {
-      std::shuffle(std::begin(shared_addr->addrs), std::end(shared_addr->addrs),
-                   randgen_);
-
       auto shared_addr_ptr = shared_addr.get();
 
       for (auto &addr : shared_addr->addrs) {