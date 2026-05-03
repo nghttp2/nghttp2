050d1374b025a8ec23eb3cb281f02e8cc8d57177
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2026-05-03 17:14:52 +0900

    nghttp: Put pseudo headers before regular headers

--- src/nghttp.cc
+++ src/nghttp.cc
@@ -437,6 +437,9 @@ submit_request(HttpClient *client, const Headers &headers, Request *req) {
     build_headers.emplace_back(kv.name, kv.value, kv.no_index);
   }
 
+  std::ranges::stable_partition(
+    build_headers, [](auto &&kv) { return kv.name.starts_with(":"); });
+
   auto nva = std::vector<nghttp2_nv>();
   nva.reserve(build_headers.size());
 
